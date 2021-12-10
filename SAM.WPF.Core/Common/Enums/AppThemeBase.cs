eeb1302088825a9ac8e5162208f4754937cba53c
Trey Morris <13120514+syntax-tm@users.noreply.github.com>
2021-12-10 03:43:18 -0500

    Minor changes to UI.

---
+++ SAM.WPF.Core/Common/Enums/AppThemeBase.cs
@@ -0,0 +1,8 @@
+﻿namespace SAM.WPF.Core
+{
+    public enum AppThemeBase
+    {
+        Light,
+        Dark
+    }
+}
---
+++ SAM.WPF/Models/AppTheme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAM.WPF.Core;
+
+namespace SAM.WPF.Models
+{
+    public class AppTheme
+    {
+
+        public AppThemeBase Base { get; set; }
+
+    }
+}
