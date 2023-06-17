2563da3f10d7a49e7a5fa809f8441745195abf85
Tobias Svenblad <59305921+tsvenbla@users.noreply.github.com>
2023-06-17 14:40:05 +0300

    Update `Callback` and `Callback<TParameter>` classes to modern C# syntax
    The `Callback` classes have been refactored to adopt modern C# practices. The changes made include:
    
    - Replace delegate `CallbackFunction` with the built-in `Action` delegate for the `OnRun` event handlers in both `Callback` and `Callback<TParameter>` classes. 
    
    - Change the `Run` method in the `Callback<TParameter>` class to check if marshalling from `nint` to `TParameter` is successful, throwing an `ArgumentException` if it isn't.
    
    - Modify properties `Id` and `IsServer` to use the `init` keyword to make them immutable after they're set.
    
    The updated code aligns with current best practices and provides some error handling.

--- SAM.API/Callbacks/Callback.cs
+++ SAM.API/Callbacks/Callback.cs
@@ -1,37 +1,42 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 
 namespace SAM.API
 {
     public abstract class Callback : ICallback
     {
-        public delegate void CallbackFunction(nint param);
+        public abstract int Id { get; init; }
+        public abstract bool IsServer { get; init; }
+        public event Action<nint>? OnRun;
+        public virtual void Run(nint param) => OnRun?.Invoke(param);
+    }
 
-        public abstract int Id { get; }
-        public abstract bool IsServer { get; }
+    public abstract class Callback<TParameter> : Callback
+        where TParameter : struct
+    {
+        private event Action<TParameter>? _onRun;
+        public event Action<TParameter>? OnRun
+        {
+            add => _onRun += value;
+            remove => _onRun -= value;
+        }
 
         public void Run(nint param)
         {
-            OnRun!(param);
+            TParameter? data = Marshal.PtrToStructure<TParameter>(param);
+            if (data.HasValue)
+            {
+                Run(data.Value);
+            }
+            else
+            {
+                throw new ArgumentException($"Failed to marshal the parameter to {nameof(TParameter)}.");
+            }
         }
 
-        public event CallbackFunction OnRun;
-    }
-
-    public abstract class Callback<TParameter> : ICallback
-        where TParameter : struct
-    {
-        public delegate void CallbackFunction(TParameter arg);
-
-        public abstract int Id { get; }
-        public abstract bool IsServer { get; }
-
-        public void Run(nint pvParam)
+        protected virtual void Run(TParameter data)
         {
-            var data = Marshal.PtrToStructure<TParameter>(pvParam)!;
-            OnRun!(data);
+            _onRun?.Invoke(data);
         }
-
-        public event CallbackFunction OnRun;
     }
 }
