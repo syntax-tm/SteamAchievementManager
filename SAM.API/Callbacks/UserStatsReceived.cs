91761b931e6e36e376623a006e9f09fd0310b676
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2023-08-22 19:14:27 -0400

    Fixed UserStatsReceived.

--- SAM.API/Callbacks/UserStatsReceived.cs
+++ SAM.API/Callbacks/UserStatsReceived.cs
@@ -2,7 +2,7 @@ using SAM.API.Stats;
 
 namespace SAM.API
 {
-    public record UserStatsReceived : Callback<UserStatsResponse>
+    public class UserStatsReceived : Callback<UserStatsResponse>
     {
         public override int Id => 1101;
         public override bool IsServer => false;
