18dd4a6072cd53b447c9ea2871c49859261c8277
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2023-08-22 19:14:26 -0400

    Refactored CallbackMessage from regular struct to C# 10.0 record struct
    Details:
    
    - Changed `CallbackMessage` from a traditional struct to a record struct. This takes advantage of C# 10.0's record feature, providing immutability and value semantics.
    
    - Replaced individual fields `User`, `Id`, `ParamPointer`, and `ParamSize` with constructor parameters in the record struct declaration. This enhances readability and succinctness.
    
    - Maintained the `StructLayout` attribute to ensure the data layout remains sequential in memory.
    
    This commit improves the code's immutability and readability while keeping the memory layout intact.

--- SAM.API/Callbacks/CallbackMessage.cs
+++ SAM.API/Callbacks/CallbackMessage.cs
@@ -1,14 +1,8 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 
 namespace SAM.API
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct CallbackMessage
-    {
-        public readonly int User;
-        public readonly int Id;
-        public readonly nint ParamPointer;
-        public readonly int ParamSize;
-    }
+    public record struct CallbackMessage(int User, int Id, nint ParamPointer, int ParamSize);
 }
