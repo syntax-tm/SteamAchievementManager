bde228c0159053810960e78ec3df9523e648b3ed
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2023-08-22 19:14:35 -0400

    Updated XmlExtensions parsing for other (non-US) locales.

--- SAM.Core/Common/Extensions/XmlExtensions.cs
+++ SAM.Core/Common/Extensions/XmlExtensions.cs
@@ -1,44 +1,40 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Globalization;
 using System.Xml;
 
 namespace SAM.Core.Extensions
 {
     public static class XmlExtensions
     {
+        private static readonly CultureInfo _enUsCulture = CultureInfo.GetCultureInfo("en-US");
+        
         public static decimal GetValueAsDecimal(this XmlNode node, string path)
         {
-            // if they didn't start with a slash, prepend two so the xpath can be
-            // relative from any node
-            if (!path.StartsWith("/"))
+            var nodeText = GetValue(node, path);
+
+            if (!decimal.TryParse(nodeText, NumberStyles.Any, CultureInfo.CurrentCulture, out var result) &&
+                !decimal.TryParse(nodeText, NumberStyles.Any, _enUsCulture, out result) &&
+                !decimal.TryParse(nodeText, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
             {
-                path = $"//{path}";
+                result = default;
             }
 
-            var selectedNode = node.SelectSingleNode(path);
-
-            Debug.Assert(selectedNode != null, nameof(selectedNode) + " != null");
-
-            return decimal.Parse(selectedNode.InnerText);
+            return result;
         }
 
         // TODO: add support for string values ("true" and "false")
         public static bool GetValueAsBool(this XmlNode node, string path)
         {
-            // if they didn't start with a slash, prepend two so the xpath can be
-            // relative from any node
-            if (!path.StartsWith("/"))
+            var nodeText = GetValue(node, path);
+            
+            if (!int.TryParse(nodeText, NumberStyles.Any, CultureInfo.CurrentCulture, out var result) &&
+                !int.TryParse(nodeText, NumberStyles.Any, _enUsCulture, out result) &&
+                !int.TryParse(nodeText, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
             {
-                path = $"//{path}";
+                result = default;
             }
 
-            var selectedNode = node.SelectSingleNode(path);
-
-            Debug.Assert(selectedNode != null, nameof(selectedNode) + " != null");
-
-            var nodeValue = int.Parse(selectedNode.InnerText);
-
-            return Convert.ToBoolean(nodeValue);
+            return Convert.ToBoolean(result);
         }
 
         public static string GetValue(this XmlNode node, string path)
