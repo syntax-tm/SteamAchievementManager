aa0fd22951ab89606ab6d9acb8cb42d7b22f922d
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2025-03-08 15:32:55 -0600

    Updated UserStatsResponse

--- src/SAM.API/Stats/UserStatsResponse.cs
+++ src/SAM.API/Stats/UserStatsResponse.cs
@@ -1,13 +1,13 @@
 ﻿using System.Runtime.InteropServices;
 
-namespace SAM.API.Stats
-{
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct UserStatsResponse
-    {
-        public ulong GameId;
-        public int Result;
+namespace SAM.API.Stats;
 
-        public bool IsSuccess => Result == 1;
-    }
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct UserStatsResponse
+{
+    public ulong GameId;
+    public int Result;
+    public ulong SteamIdUser;
+
+    public readonly bool IsSuccess => Result == 1;
 }
