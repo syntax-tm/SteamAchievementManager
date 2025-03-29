fa4d88919af2163fb2416d16bb42ff7104b7fb34
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2025-03-28 22:23:42 -0500

    Converted to file-scoped namespace

--- src/SAM/Common/SplashScreen/SplashScreenHelper.cs
+++ src/SAM/Common/SplashScreen/SplashScreenHelper.cs
@@ -3,67 +3,66 @@ using System.Windows.Threading;
 using SAM.Core;
 using Wpf.Ui.Controls;
 
-namespace SAM.SplashScreen
+namespace SAM.SplashScreen;
+
+public static class SplashScreenHelper
 {
-    public static class SplashScreenHelper
+    private static Thread _thread;
+    private static volatile bool _isInitialized;
+    private static volatile FluentWindow _splashWindow;
+    private static volatile SplashScreenViewModel _splashScreenVm;
+    
+    public static void Init(string status = null)
     {
-        private static Thread _thread;
-        private static volatile bool _isInitialized;
-        private static volatile FluentWindow _splashWindow;
-        private static volatile SplashScreenViewModel _splashScreenVm;
-        
-        public static void Init(string status = null)
+        if (_isInitialized) throw new SAMException();
+
+        var pts = new ParameterizedThreadStart(ThreadStartingPoint);
+
+        _thread = new (pts);
+        _thread.SetApartmentState(ApartmentState.STA);
+        _thread.IsBackground = true;
+        _thread.Start(status);
+
+        _isInitialized = true;
+    }
+
+    public static void SetStatus(string status = null)
+    {
+        _splashScreenVm.Status = status;
+    }
+
+    private static void ThreadStartingPoint(object arg = null)
+    {
+        _splashScreenVm = SplashScreenViewModel.Create();
+        _splashScreenVm.Status = arg?.ToString();
+
+        _splashWindow = new SplashScreenView();
+        _splashWindow.DataContext = _splashScreenVm;
+
+        _splashWindow.Show();
+
+        Dispatcher.Run();
+    }
+
+    public static void Show(string status = null)
+    {
+        if (!_isInitialized)
         {
-            if (_isInitialized) throw new SAMException();
+            Init(status);
 
-            var pts = new ParameterizedThreadStart(ThreadStartingPoint);
-
-            _thread = new (pts);
-            _thread.SetApartmentState(ApartmentState.STA);
-            _thread.IsBackground = true;
-            _thread.Start(status);
-
-            _isInitialized = true;
+            return;
         }
 
-        public static void SetStatus(string status = null)
+        _splashScreenVm.Status = status;
+    }
+
+    public static void Close()
+    {
+        _splashWindow?.Dispatcher.BeginInvoke(() =>
         {
-            _splashScreenVm.Status = status;
-        }
+            _splashWindow.Close();
 
-        private static void ThreadStartingPoint(object arg = null)
-        {
-            _splashScreenVm = SplashScreenViewModel.Create();
-            _splashScreenVm.Status = arg?.ToString();
-
-            _splashWindow = new SplashScreenView();
-            _splashWindow.DataContext = _splashScreenVm;
-
-            _splashWindow.Show();
-
-            Dispatcher.Run();
-        }
-
-        public static void Show(string status = null)
-        {
-            if (!_isInitialized)
-            {
-                Init(status);
-
-                return;
-            }
-
-            _splashScreenVm.Status = status;
-        }
-
-        public static void Close()
-        {
-            _splashWindow?.Dispatcher.BeginInvoke(() =>
-            {
-                _splashWindow.Close();
-
-                _splashWindow.Dispatcher.InvokeShutdown();
-            }, DispatcherPriority.Background);
-        }
+            _splashWindow.Dispatcher.InvokeShutdown();
+        }, DispatcherPriority.Background);
     }
 }
